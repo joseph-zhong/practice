a1437dd082f6a624df1af8759bca842f67b2f2a9
Joseph Zhong <josephz@cs.washington.edu>
2018-11-22 14:27:34 -0800

    ctci ch1.4: palindrome permutation

---
+++ ctci/ch1_arrays_strings/palindrome_permutation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def isPermutationOfPalindrome(s1):
+    """ Returns whether the string can be composed as a palindrome.
+
+    Approach: A palindrome of even length must have character counts that are all even, and if a palindrome is of odd
+    length, then only one character can be of odd count. We thus construct a character count by iterating through the
+    string. """
+
+    charCount = dict()
+    for char in s1:
+        if char not in charCount:
+            charCount[char] = 0
+        charCount[char] += 1
+
+    count = 0
+    for char in charCount:
+        count += charCount[char] % 2
+    return count <= 1
+
+print(isPermutationOfPalindrome('aba') == True)
+print(isPermutationOfPalindrome('aer') == False)
+print(isPermutationOfPalindrome('aaabbbaaa') == True)
+print(isPermutationOfPalindrome('aaabbbaa') == False)
+print(isPermutationOfPalindrome('aaabbaa') == True)
+
