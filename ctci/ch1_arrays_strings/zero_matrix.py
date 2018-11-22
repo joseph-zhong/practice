7557e422f9e6e5648af5a5479945643c8f84351e
Joseph Zhong <josephz@cs.washington.edu>
2018-11-22 14:58:36 -0800

    ctci 1.8: zero_matrix optimization

--- ctci/ch1_arrays_strings/zero_matrix.py
+++ ctci/ch1_arrays_strings/zero_matrix.py
@@ -27,3 +27,45 @@ def zero_matrix(mat):
     for c in colsToModify:
         mat[:, c] = 0
 
+# Optimization: Rather than using O(n + m) space to track the rows and columns needed to modify, we can use the first row
+# and column to track our necessary row and column modifications and modify the first row and column if necessary last.
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        # Flag to check whether to it's necessary to modify our first column.
+        set_header_to_zero = False
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for r in range(rows):
+            if matrix[r][0] == 0:
+                set_header_to_zero = True
+
+            for c in range(1, cols):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+
+        # If the first column in the first row had a zero, then the whole row must be changed.
+        if matrix[0][0] == 0:
+            for c in range(cols):
+                matrix[0][c] = 0
+
+        # If any value in the first column had a zero, the whole column must be changed.
+        if set_header_to_zero:
+            for r in range(rows):
+                matrix[r][0] = 0
+
+s = Solution()
+mat = [[1,1,1], [1,0,1], [1,1,1]]
+s.setZeroes(mat)
+print(mat == [[1,0,1],[0,0,0],[1,0,1]])
+
